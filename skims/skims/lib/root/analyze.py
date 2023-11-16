74a95f5bac6e54ffa1d2416b8d989f99a395097d
John Cognetti <jcognetti@adventive.com>
2023-11-15 23:33:23 -0500

    increase timeout to 8 hours

--- skims/skims/lib/root/analyze.py
+++ skims/skims/lib/root/analyze.py
@@ -101,7 +101,7 @@ def analyze(
                 stores=stores,
                 worker=worker,
             )
-            _, f_failed = wait(futures, 3600 * 4) # 4 HOUR TIMEOUT!? CRAZINESS! 60s wasn't enough time for a Nodejs project built with Parcel...
+            _, f_failed = wait(futures, 3600 * 8) # 4 HOUR TIMEOUT!? CRAZINESS! 60s wasn't enough time for a Nodejs project built with Parcel...
             if f_failed and not has_failed:
                 has_failed = True
                 log_blocking("info", 'scan failed due to timeout: %s', path)
